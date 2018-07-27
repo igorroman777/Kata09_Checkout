98786eb2688e97fc1e8daa78a181980be6270f94
iroman777 <info@artwista.de>
2018-07-27 10:53:09 +0200

    5. commit: .yaml file for rules

--- src/main/java/com/roman/pricing/CheckOut.java
+++ src/main/java/com/roman/pricing/CheckOut.java
@@ -3,28 +3,34 @@ package com.roman.pricing;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.jeasy.rules.api.Rules;
+
 public class CheckOut {
-    private final PricingEngine pricingEngine = new PricingEngine();
-    private final Map<String, Item> items = new HashMap<>();
+	private final PricingEngine pricingEngine = new PricingEngine();
+	private final Map<String, Item> items = new HashMap<>();
 
-    public void scan(String scannedValue) {
-    	Item scannedItem = new Item(scannedValue);
-    	Item item = items.get(scannedItem.getValue());
-    	if(item == null) {
-    		scannedItem.setQuantity(1);
-    		items.put(scannedValue, scannedItem);
-    	} else {
-        	Integer quantity = item.getQuantity();
-            item.setQuantity(++ quantity);
-    	}
-    }
+	public void initializeRule(Rules rules) {
+		pricingEngine.initialize(rules);
+	}
 
-    public Integer total() {
-    	Integer totalPrice = new Integer(0);
-    	for(Item item: items.values()) {
-    		totalPrice = totalPrice + pricingEngine.calculatePrice(item).getPrice();
-    	}
-    	return totalPrice;
-    }
+	public void scan(String scannedValue) {
+		Item scannedItem = new Item(scannedValue);
+		Item item = items.get(scannedItem.getValue());
+		if (item == null) {
+			scannedItem.setQuantity(1);
+			items.put(scannedValue, scannedItem);
+		} else {
+			Integer quantity = item.getQuantity();
+			item.setQuantity(++quantity);
+		}
+	}
+
+	public Integer total() {
+		Integer totalPrice = new Integer(0);
+		for (Item item : items.values()) {
+			totalPrice = totalPrice + pricingEngine.calculatePrice(item);
+		}
+		return totalPrice;
+	}
 
 }
